f8e88a619f856d6e11ba25950a0d028065e4fc36
2200032547-Giridhar <161588814+2200032547-Giridhar@users.noreply.github.com>
2025-11-30 11:39:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import CustomerDashboard from "./pages/CustomerDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { Role } from "./types";
 
 function App() {
   const [role, setRole] = useState<Role | null>(null);
 
+  // Load role on refresh
+  useEffect(() => {
+    const saved = localStorage.getItem("role") as Role | null;
+    if (saved) setRole(saved);
+  }, []);
+
+  function handleLogin(role: Role) {
+    setRole(role);
+    localStorage.setItem("role", role);
+  }
+
   return (
     <Routes>
-      <Route path="/login" element={<Login onLogin={setRole} />} />
+      <Route path="/login" element={<Login onLogin={handleLogin} />} />
+
       <Route
         path="/customer"
         element={
@@ -21,12 +33,18 @@ function App() {
           )
         }
       />
+
       <Route
         path="/admin"
         element={
-          role === "ADMIN" ? <AdminDashboard /> : <Navigate to="/login" replace />
+          role === "ADMIN" ? (
+            <AdminDashboard />
+          ) : (
+            <Navigate to="/login" replace />
+          )
         }
       />
+
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
